3dd7d359d7db50b9303e646baea3718166b88839
Sen-Yam <96739035+Sen-Yam@users.noreply.github.com>
2022-03-10 11:43:36 +0000

    name change

--- app.js
+++ app.js
@@ -3,18 +3,23 @@
 express=require('express') ;
 var app=express() ; 
 app.set('view engine','ejs'); 
-app.listen(2000 , 'localhost' , (err) => {
+app.listen(2000 , 'localhost' ,(err) => {
     if(err) throw err;
 });
 
 app.get('/'  , (req , res) => {
-    res.render('codeejs');
+    res.render('codeejs' , {array : [
+        {name: "LARABA" , surname : "Yamina" , age: "19 years old"},
+        {name: "LARABA2" , surname : "Yamina2" , age: "19 years old"},
+        {name: "LARABA3" , surname : "Yamina3" , age: "19 years old"}
+        
+        
+    ]});
     
 });
-
-app.get('/filehtml'  , (req , res) => {
-    res.sendFile('./filehtml.html' , {root : __dirname});
-});
+app.get('/filehtml' ,(req , res) => {
+    res.render('file');
+})
 app.use((req , res) => {
     res.sendFile('./filehtml.html' , {root : __dirname}) ;
 });
